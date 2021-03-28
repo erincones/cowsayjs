2946f0aa21be21be9667f59c44ff20b1e0240d31
Erick Rincones <erick.rincones@ciens.ucv.ve>
2021-03-28 00:39:14 -0400

    project setup

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * A nodejs clone of the classic cowsay and cowthink cli commands.
+ */
+declare module "cowsayjs" {
+  export default function(): string;
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+/**
+ * A nodejs clone of the classic cowsay and cowthink cli commands
+ *
+ * @module cowsayjs
+ */
+module.exports = function () {
+  return "moo";
+};
